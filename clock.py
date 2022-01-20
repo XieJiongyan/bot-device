3b11a55e64c6a67212de2f2ae5a4ee0a7c342389
Xiejiongyan <joy2home@outlook.com>
2022-01-20 20:27:20 +0800

    J.0.1.0 done divice net

--- clock.py
+++ clock.py
@@ -1,13 +1,24 @@
 import json 
 import tcp
 import asyncio
+from crontab import CronTab
+from apscheduler.schedulers.blocking import BlockingScheduler
+from apscheduler.triggers.cron import CronTrigger
 
 clocks = [{}]
+aps = BlockingScheduler()
+def execAClock(text: str):
+  print(text)
 
 def getFromNet(extras: str):
-  global clocks
+  global clocks, aps
+  aps.remove_all_jobs()
   d = json.loads(extras) 
   clocks = d["clocks"]
+  for clock in clocks:
+    if ~clock['is_active']:
+      continue
+    aps.add_job(execAClock, CronTrigger.from_crontab(clock['cron']), args=[clock['text']])
 
 async def printFromNet():
   while True:
--- main.py
+++ main.py
@@ -1,2 +1,17 @@
 import tcp 
 import clock 
+import asyncio 
+
+async def listen():
+  while True:
+    re = tcp.read()
+    if re == "" or re == b"":
+      break
+    if re["command"] == "device" and re["options"] == ["total"]:
+      clock.getFromNet(re["extras"])
+      print(clock.clocks)
+      
+if __name__ == "__main__":
+  tcp.Login()
+  tcp.write({"command":"clock", "options":["get"], "extras": None})
+  asyncio.run(listen())
